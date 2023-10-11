c04c67fa48402fc0b68521edec5a3d5b0ea5d83c
David Tielke <david.tielke@hotmail.de>
2023-10-11 14:44:28 +0200

    DI Kernel eingebaut

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DavidTielke.PersonManagerApp.Data.DataStoring.Csv;
 using DavidTielke.PersonManagerApp.Logic.PersonManagement;
+using Ninject;
 
 namespace DavidTielke.PersonManagerApp.UI.ConsoleClient;
 
@@ -7,10 +8,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var loader = new FileLoader();
-        var parser = new PersonCsvParser();
-        var repository = new PersonRepository(loader, parser);
-        var manager = new PersonManager(repository);
+        var kernel = new StandardKernel();
+
+        kernel.Bind<IPersonManager>().To<PersonManager>();
+        kernel.Bind<IPersonRepository>().To<PersonRepository>();
+        kernel.Bind<IPersonParser>().To<PersonCsvParser>();
+        kernel.Bind<IFileLoader>().To<FileLoader>();
+
+        var manager = kernel.Get<IPersonManager>();
 
         var adults = manager.GetAllAdults().Take(2).ToList();
         Console.WriteLine($"### Erwachsene ({adults.Count}) ###");
